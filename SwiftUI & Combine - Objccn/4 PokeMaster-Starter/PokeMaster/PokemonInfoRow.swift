b22a06b039945ad02041aea2fc9c2d096b23523d
Ficow <945655645@qq.com>
2021-12-15 08:47:05 +0800

    Update SwiftUI demo;

---
+++ SwiftUI & Combine - Objccn/4 PokeMaster-Starter/PokeMaster/PokemonInfoRow.swift
@@ -0,0 +1,90 @@
+//
+//  PokemonInfoRow.swift
+//  PokeMaster
+//
+//  Created by Ficow on 2021/12/15.
+//  Copyright © 2021 OneV's Den. All rights reserved.
+//
+
+import SwiftUI
+
+struct PokemonInfoRow: View {
+    let model = PokemonViewModel.sample(id: 1)
+
+    var body: some View {
+        VStack {
+            HStack {
+                Image("Pokemon-\(model.id)")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .aspectRatio(contentMode: .fit)
+                    .shadow(radius: 4)
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text(model.name)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.white)
+                    Text(model.nameEN)
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                }
+            }
+            .padding(.top, 12)
+            HStack(spacing: 20) {
+                Spacer()
+                Button(action: { print("fav") }) {
+                    Image(systemName: "star")
+                        .modifier(ToolButtonModifier())
+//                        .font(.system(size: 25))
+//                        .foregroundColor(.white)
+//                        .frame(width: 30, height: 30)
+                }
+                Button(action: { print("panel") }) {
+                    Image(systemName: "chart.bar")
+                        .modifier(ToolButtonModifier())
+                }
+                Button(action: { print("web") }) {
+                    Image(systemName: "info.circle")
+                        .modifier(ToolButtonModifier())
+                }
+            }
+            .padding(.bottom, 12)
+        }
+        .frame(height: 120)
+        .padding(.leading, 23)
+        .padding(.trailing, 15)
+        .background(
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(model.color, style: StrokeStyle(lineWidth: 4))
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(
+                                colors: [.white, model.color]
+                            ),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                    )
+            }
+        )
+        .padding(.horizontal)
+    }
+}
+
+struct ToolButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 25))
+            .foregroundColor(.white)
+            .frame(width: 30, height: 30)
+    }
+}
+
+struct PokemonInfoRow_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonInfoRow()
+    }
+}
