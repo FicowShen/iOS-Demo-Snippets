60ec8e91325865b34551987a5f70a4b5fa0ab2d4
Ficow Shen <ficowshen@hotmail.com>
2021-12-19 13:42:47 +0800

    Combine operators;

--- SwiftUI & Combine - Objccn/CombineExample-Starter.playground/Pages/Basic.xcplaygroundpage/Contents.swift
+++ SwiftUI & Combine - Objccn/CombineExample-Starter.playground/Pages/Basic.xcplaygroundpage/Contents.swift
@@ -1,5 +1,87 @@
 import Combine
 
+enum MyError: Error {
+    case myError
+}
+
+
+check("Catch and Continue") {
+    ["1", "2", "Switf", "4"]
+        .publisher
+        .print("\n[Original]")
+        .flatMap { s in
+            return Just(s)
+                .tryMap { s -> Int in
+                    guard let value = Int(s) else {
+                        throw MyError.myError
+                    }
+                    return value
+                }
+                .print("[TryMap]")
+                .catch { _ in
+                    Just(-1).print("[Just]")
+                }
+                .print("[Catch]")
+        }
+}
+
+
+check("Catch with Another Publisher") {
+    ["1", "2", "Swift", "4"]
+        .publisher
+        .tryMap { s -> Int in
+            guard let value = Int(s) else {
+                throw MyError.myError
+            }
+            return value
+        }
+        .catch { _ in [-1, -2, -3].publisher }
+}
+
+
+check("Catch with Just") {
+    ["1", "2", "Swift", "4"]
+        .publisher
+        .tryMap { s -> Int in
+            guard let value = Int(s) else {
+                throw MyError.myError
+            }
+            return value
+        }
+        .catch { _ in Just(-1) }
+}
+
+
+check("Replace Error") {
+    ["1", "2", "Swift", "4"]
+        .publisher
+        .tryMap { s -> Int in
+            guard let value = Int(s) else {
+                throw MyError.myError
+            }
+            return value
+        }
+        .replaceError(with: -1)
+}
+
+
+check("Throw") {
+    ["1", "2", "Swift", "4"]
+        .publisher
+        .tryMap { s -> Int in
+            guard let value = Int(s) else {
+                throw MyError.myError
+            }
+            return value
+        }
+}
+
+
+check("Fail") {
+    Fail<Int, SampleError>(error: .sampleError)
+        .mapError { _ in MyError.myError }
+}
+
 
 check("Remove Duplicates") {
     ["S", "Sw", "Sw", "Sw", "Swi",
