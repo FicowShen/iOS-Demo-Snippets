10861fa61db81cee8e384e2b9e28970395dd2a1a
Ficow Shen <ficowshen@hotmail.com>
2023-09-18 09:52:53 +0800

    Add more rules;

--- SwiftLintDemo/SwiftLintDemo/ViewController.swift
+++ SwiftLintDemo/SwiftLintDemo/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController   :  UIViewController {
+
+    // explicit_init
+    private(set) var viewModel = ViewModel.init(title: String.init("Test"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        self.title = viewModel.title
     }
 
+    func convertValues(a: Int ,  b: Int,  c: Int) -> [String] { // comma, identifier_name
+        return [a, b, c].compactMap { String.init($0) } // explicit_init
+    }
+} // trailing_newline
 
-}
 
---
+++ SwiftLintDemo/SwiftLintDemo/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModel.swift
+//  SwiftLintDemo
+//
+//  Created by Ficow on 2023/9/18.
+//
+
+import Foundation
+import Dispatch
+import SwiftShims
+
+struct ViewModel {
+    let title :  String // colon
+}
